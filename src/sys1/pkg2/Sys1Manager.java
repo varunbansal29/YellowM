6bdca4ace20503d1c441985a0c7907e355656a97
julian.dragoi <julian.dragoi@GBR008998-D.corp.jato.com>
2016-12-13 17:11:06 +0000

    Override notation added

--- src/sys1/pkg2/Sys1Manager.java
+++ src/sys1/pkg2/Sys1Manager.java
@@ -6,13 +6,14 @@
 package sys1.pkg2;
 
 import java.util.HashMap;
+import java.util.Set;
 
 
 /**
  *
  * @author julian.dragoi
  */
-public class Sys1Manager
+public class Sys1Manager implements Sys1Interface
 {
    HashMap<String,Project> allProjects;
    HashMap<String,Employee> allEmployees;
@@ -24,18 +25,21 @@ public class Sys1Manager
    
    
    
+   @Override
    public void addEmployeeToList(String empid, Employee emp){
        
       allEmployees.put(empid,emp);
      
    }
    
+   @Override
    public void removeEmployee(String empid, Employee emp){
        
        allEmployees.remove(empid,emp);
    }
    
    
+   @Override
    public Employee getEmployee(Employee emp){
        
        if(emp != null){
@@ -46,7 +50,48 @@ public class Sys1Manager
        
    }
    
-   //public void getAllProjects()
+   @Override
+   public void getAllEmployees(){
+       
+       Set<String> keys = allEmployees.keySet();
+       for(String emp: keys){
+            System.out.println(emp);
+        }
+   }
+           
+   @Override
+ public void addProjectToList(String projid, Project proj){
+       
+      allProjects.put(projid,proj);
+     
+   }
    
-   //public void getAllEmployees()
+   @Override
+ public void removeProjectToList(String projid, Project proj){
+       
+      allProjects.remove(projid,proj);
+     
+   }
+   
+   
+   @Override
+   public Project getProject(Project proj){
+       
+       if(proj != null){
+           return proj;
+       }
+       else
+       return null;
+       
+   }
+   
+   @Override
+   public void getAllProjects(){
+       
+       Set<String> keys = allProjects.keySet();
+       for(String proj: keys){
+            System.out.println(proj);
+        }
+   }
+       
 }
